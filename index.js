8a3989c9f07e2806536ec51552897f7e1da12fd4
Adam Kearney <aklives@hotmail.com>
2019-02-23 13:40:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(number){
   var array = [];
   do{
-    array.push('I love the Beatles')
-  }
+    array.push('I love the Beatles!');
+    number++;
+  } while (number<15)
 }
