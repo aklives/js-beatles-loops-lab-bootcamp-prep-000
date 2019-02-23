ae55c0b551f9bbc2cccfc34bc3192502a2081e36
Adam Kearney <aklives@hotmail.com>
2019-02-23 13:28:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 
+facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(facts){
-  
+  i=0;
+  while (i < facts.length){
+    facts[i] = facts[i] + '!';
+    i++;
+  }
 }
