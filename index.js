c349f6f3ff9052cb93d61f677df185d998c62afd
Adam Kearney <aklives@hotmail.com>
2019-02-23 13:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ facts = [
 function johnLennonFacts(facts){
   var i=0;
   while (i < facts.length){
-    facts[i] = facts[i] + '!';
+    facts[i] = facts[i] + '!!!';
     i++;
   }
   return facts;
